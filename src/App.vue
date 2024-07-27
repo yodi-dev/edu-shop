fbc5b22c4ec0fb970ba0bd16b9f516e58d617465
irawanyodi <dykhazta@gmail.com>
2024-07-27 21:15:32 +0700

    initial set

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
+  <header>
+    <Navbar/>
+  </header>
   <main>
   </main>
 </template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav>
+        <h1>Edu-shop</h1>
+    </nav>
+</template>
+
+<style scoped>
+nav {
+    width: 100%;
+    height: 10vh;
+    background-color: antiquewhite;
+}
+
+</style>
