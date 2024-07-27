7edba95e159457d8653bb25b7b198d8680e0a26a
irawanyodi <dykhazta@gmail.com>
2024-07-27 22:59:49 +0700

    create navbar

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ import Navbar from './components/Navbar.vue';
     <Navbar/>
   </header>
   <main>
+    
   </main>
 </template>
 
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,28 @@
 <template>
     <nav>
+        <img src="../assets/logo.png" alt="">
         <h1>Edu-shop</h1>
     </nav>
 </template>
 
 <style scoped>
 nav {
-    width: 100%;
-    height: 10vh;
-    background-color: antiquewhite;
+    display: flex;
+    width: 100vmax;
+    height: 15vh;
+    /* padding: 0 auto; */
+    background-color: #ffeaa7;
 }
 
+nav img {
+    margin: auto 1rem;
+    height: 4em;
+    /* height: auto; */
+}
+
+nav h1 {
+    margin: auto 0;
+    color: #00b894;
+    font-weight: bold;
+}
 </style>
